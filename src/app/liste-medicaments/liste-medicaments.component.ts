101e9a26a6457c7ff2a06d9a10e4b56fdda322e4
Shanks lll <shanksx00@gmail.com>
2022-05-29 20:20:13 +0200

    liste medicaments

--- src/app/liste-medicaments/liste-medicaments.component.ts
+++ src/app/liste-medicaments/liste-medicaments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-liste-medicaments',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-medicaments.component.css']
 })
 export class ListeMedicamentsComponent implements OnInit {
+  displayedColumns: string[] = ['Nom', 'Photo', 'Description', 'Categorie'];
+  listMedicaments: any = []
 
-  constructor() { }
+  constructor(private ApiService: ApiService) { }
 
   ngOnInit(): void {
+    this.ApiService.GetAllMedicaments().subscribe(Result => {
+      this.listMedicaments = Result
+    })
   }
 
 }
